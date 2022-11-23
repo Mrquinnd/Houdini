bdb0b9d17f524b5907dd599b3a17b413fe1faba2
Quinn Dacre <45511679+Mrquinnd@users.noreply.github.com>
2022-11-24 03:29:33 +1300

    Selection based MaterialX converter.
    Connecting displacement node to main material shader.

--- MTLX_converter.py
+++ MTLX_converter.py
@@ -1,3 +1,6 @@
+#MTLK Converter converts existing principled shaders into MaterialX networks which run faster and more efficiently particularly when using Solaris and Karma renderer.
+
+#import mod
 import hou
 
 mysel = hou.selectedNodes()[0]
@@ -5,11 +8,28 @@ mysel = hou.selectedNodes()[0]
 matcontext = mysel.parent()
 
 #define output surface
-matsubnet = matc ontext.createNode("subnet", mysel.name() + "_materialX")
+matsubnet = matcontext.createNode("subnet", mysel.name() + "_materialX")
 
 surfaceoutput = matsubnet.createNode("subnetconnector", "surface_output")
+surfaceoutput.parm("parmname").set("surface")
+surfaceoutput.parm("parmlabel").set("Surface")
+surfaceoutput.parm("parmtype").set("surface")
+surfaceoutput.parm("connectorkind").set("output") 
 
 #define output displacement
-surfaceoutput.parm("parmname").set
+dispoutput = matsubnet.createNode("subnetconnector", "surface_output")
+dispoutput.parm("parmname").set("Displacement")
+dispoutput.parm("parmlabel").set("Displacement")
+dispoutput.parm("parmtype").set("displacement")
+dispoutput.parm("connectorkind").set("output") 
+
+#Create Albedo
+path = mysel.parm("basecolor_texture").eval()
+matsubnet.createNode("mtlximage", "Albedo")
+albedo.parm("file").set(path)
+mtlx.setInput(1, albedo)
 
 #Create MTLX STD
+mtlx = matsubnet.createNode("mtlxstandard_surface", "surface_mtlx")
+surfaceoutput.setInput(0, mtlx)
+matsubnet.layoutChildren()
