4bda531d85b3360bac9095f15604af393a1c0e62
Quinn Dacre <45511679+Mrquinnd@users.noreply.github.com>
2022-11-24 22:30:21 +1300

    Update Lighting_HDA.py
    Connected lights with menu.

--- Lighting_HDA.py
+++ Lighting_HDA.py
@@ -20,19 +20,22 @@ node = root.collapseIntoSubnet(sel, sel[0].name())
 group = node.parmTemplateGroup()
 
 #Create a folder "My Parms"
-folder = hou.FolderParmTemplate("folder", "My Parms")
+folder = hou.FolderParmTemplate("folder", "THREE POINT LIGHT CONTROL")
 folderSettings = hou.FolderParmTemplate("folderSettings", "Settings")
 #create a float slider
 intensity = hou.FloatParmTemplate("intensity", "Intensity", 1, (1, 0, 0),0,10)
 exposure = hou.FloatParmTemplate("exposure", "Exposure", 1, (0, 0, 0),0,10)
 enable = hou.ToggleParmTemplate("enable", "Enable",1, help = "Enable all my lights")
 radius = hou.FloatParmTemplate("radius", "Radius", 2, (1, 1, 0),0,10)
+color = hou.FloatParmTemplate("color", "Color", 3, default_value=(1, 1, 1), look = hou.parmLook.ColorSquare, naming_scheme = hou.parmNamingScheme.RGBA)
 
 
 #Add intensity to folder
 folder.addParmTemplate(enable)
 folderSettings.addParmTemplate(intensity)
 folderSettings.addParmTemplate(exposure)
+folderSettings.addParmTemplate(radius)
+folderSettings.addParmTemplate(color)
 
 folder.addParmTemplate(folderSettings)
 #Add our folder into the parmTemplate of Geo
@@ -49,3 +52,17 @@ condition = (hou.parmCondType.HideWhen,'{ enable == 0}' )
 
 folderSettings.setConditional(condition[0],condition[1])
 node.replaceSpareParmTuple(folderSettings.name(), folderSettings)
+
+###
+for lgt in node.children():
+    lgt.parm("light_intensity").setExpression('ch("../intensity")')
+    lgt.parm("light_exposure").setExpression('ch("../exposure")')
+    lgt.parm("light_enable").setExpression('ch("../enable")')
+
+    lgt.parm("light_colorr").setExpression('ch("../colorr")')
+    lgt.parm("light_colorg").setExpression('ch("../colorg")')
+    lgt.parm("light_colorb").setExpression('ch("../colorb")')
+    
+    lgt.parm("areasize1").setExpression('ch("../radiusx")')
+    lgt.parm("areasize2").setExpression('ch("../radiusy")')
+
