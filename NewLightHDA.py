8c00942d96762659191185b1d91f0d729dab4887
Quinn Dacre <45511679+Mrquinnd@users.noreply.github.com>
2023-01-29 01:18:25 +1300

    Create NewLightHDA.py

---
+++ NewLightHDA.py
@@ -0,0 +1,81 @@
+import hou
+
+#creating geo node
+
+sel = hou.selectedNodes()
+
+import hou
+
+#creating geo node
+
+sel = hou.selectedNodes()
+
+root = sel[0].parent()
+
+node = root.collapseIntoSubnet(sel, sel[0].name())
+
+
+
+#parmtemplate group class
+group = node.parmTemplateGroup()
+
+#Create a folder "My Parms"
+folder = hou.FolderParmTemplate("folder", "THREE POINT LIGHT CONTROL")
+folderSettings = hou.FolderParmTemplate("folderSettings", "Settings")
+#create a float slider
+light_position = hou.FloatParmTemplate("light_position", "Light Position", 1, (1, 0, 0),0,10)
+y_angle = hou.FloatParmTemplate("y_angle", "Y Angle", 1, (1, 0, 0),0,360)
+
+
+intensity = hou.FloatParmTemplate("intensity", "Intensity", 1, (1, 0, 0),0,10)
+exposure = hou.FloatParmTemplate("exposure", "Exposure", 1, (0, 0, 0),0,10)
+enable = hou.ToggleParmTemplate("enable", "Enable",1, help = "Enable all my lights")
+radius = hou.FloatParmTemplate("radius", "Radius", 2, (1, 1, 0),0,10)
+color = hou.FloatParmTemplate("color", "Color", 3, default_value=(1, 1, 1), look = hou.parmLook.ColorSquare, naming_scheme = hou.parmNamingScheme.RGBA)
+
+
+#Add intensity to folder
+folder.addParmTemplate(enable)
+folderSettings.addParmTemplate(light_position)
+
+folderSettings.addParmTemplate(intensity)
+folderSettings.addParmTemplate(exposure)
+folderSettings.addParmTemplate(radius)
+folderSettings.addParmTemplate(color)
+folderSettings.addParmTemplate(y_angle)
+
+folder.addParmTemplate(folderSettings)
+
+#Add our folder into the parmTemplate of Geo
+group.append(folder)
+
+group.hideFolder("Transform", 1)
+group.hideFolder("Subnet",1)
+
+#Assigning the group template into the geo tabs
+
+node.setParmTemplateGroup(group)
+
+condition = (hou.parmCondType.HideWhen,'{ enable == 0}' )
+
+folderSettings.setConditional(condition[0],condition[1])
+node.replaceSpareParmTuple(folderSettings.name(), folderSettings)
+
+###
+for lgt in node.children():
+    
+    lgt.parm("ry").setExpression('ch("../y_angle")')
+
+    lgt.parm("ty").setExpression('ch("../light_position")')
+
+
+    lgt.parm("light_intensity").setExpression('ch("../intensity")')
+    lgt.parm("light_exposure").setExpression('ch("../exposure")')
+    lgt.parm("light_enable").setExpression('ch("../enable")')
+
+    lgt.parm("light_colorr").setExpression('ch("../colorr")')
+    lgt.parm("light_colorg").setExpression('ch("../colorg")')
+    lgt.parm("light_colorb").setExpression('ch("../colorb")')
+    
+    lgt.parm("areasize1").setExpression('ch("../radiusx")')
+    lgt.parm("areasize2").setExpression('ch("../radiusy")')
