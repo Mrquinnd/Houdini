26464942ced74c85f3a9010b330910d6c0ddc732
Quinn Dacre <45511679+Mrquinnd@users.noreply.github.com>
2024-07-28 14:57:35 +1200

    Update mtlxUSD.py

--- mtlxUSD.py
+++ mtlxUSD.py
@@ -1,35 +1,26 @@
 import hou
 import os
 
-#Channels for substance painter
 textureChannels = ['BaseColor', 'Roughness', 'Height', 'Opacity', 'Metalness']
 
-#set the base name with prompt ("ex: watch_v2_wood")
-baseName = hou.ui.readInput( initial_contents="Rover4OutsideUVOnly" , message="Set the base object name", buttons= ["Save","Cancel"], title="Object BaseName")
-#print(baseName)
+selected_nodes = hou.selectedNodes()
 
-if baseName[0]==0 and baseName[1] != '' :
+if not selected_nodes:
+    raise Exception("Please select a Material Library node.")
+else:
+    print("Selected nodes:", selected_nodes)
 
-    #set the textures directory with file input
-    currentproject = os.path.dirname(hou.hipFile.path())
-    
-    pathToTextures = hou.ui.selectFile( file_type=hou.fileType.Directory, start_directory = currentproject, title = "Select the texture folder")
-    
+material_library_node = selected_nodes[0]
+print("Selected Material Library node:", material_library_node)
+
+if material_library_node.type().name() != "materiallibrary":
+    raise Exception("Selected node is not a Material Library node.")
+else:
+    print("The selected node is a Material Library node.")
+
     selection = hou.selectedNodes()[0]
     
     context = selection.parent()
     
-    materialx = selection.createNode("mtlxstandard_surface", baseName[1]+"_mtlx")
+    materialx = selection.createNode("mtlxstandard_surface", "_mtlx")
     
-    UVNode = selection.createNode("mtlxtexcoord","UVS")
-    UVNode.parm("signature").set("vector2")
-    
-    for texture in os.listdir(pathToTextures):
-        for channel in textureChannels:
-            if baseName[1] in texture and channel in texture:
-                print(texture)
-
-    
-    print(pathToTextures)
-    
-    print(currentproject)
